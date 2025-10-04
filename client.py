7f6187cac3e64b4b478ade7718ea2887f3b76dae
AbdulRaffayGill <abdulraffaykhadimgill@gmail.com>
2025-10-04 09:23:08 +0500

    Initiates a TCP connection to a server, then send and receive messages

---
+++ client.py
@@ -0,0 +1,39 @@
+import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host=str(input("Enter the Private Ip of server\n"))
+if not host:
+    host=socket.gethostname()
+s.connect((host,5000))
+r='0'
+arr=['/','*',':',';','!','@','#','$','%','^','&','?','|','\\']
+name=str(input("Enter your name\n"))
+while True:
+    if name not in arr:
+        break
+    else:
+        print("Only special chracters not allowed\n")
+        name=str(input("Enter your name:\n"))
+s.sendall(name.encode('utf-8'))
+name_r=s.recv(100).decode().strip()
+while True:
+    i=str(input(name+" : "))
+    i=i.encode('utf-8')
+    send=s.sendall(i)
+    r=s.recv(5000)
+    r=r.decode().strip()
+    if not r:
+        print(name_r," : ","(Empty message received\n)")
+    elif r==';':
+        print("Chat ended by the other person:\nQuitting\n")
+        s.close()
+        break
+    else:
+        print(name_r," : ",r,'\n')
+    i=str(input(name+" : "))
+    i=i.encode('utf-8')
+    send=s.sendall(i)
+    if i==";":
+        print("You have closed the chat\n")
+        s.close()
+        break
+        
